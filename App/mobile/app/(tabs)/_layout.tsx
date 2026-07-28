de6a08281d094eda942e7f95ddad2b6270a6ab8c
eduu <everton.edu.2009@gmail.com>
2026-07-27 21:03:59 -0300

    Alterado como alterar as paginas no app

---
+++ App/mobile/app/(tabs)/_layout.tsx
@@ -0,0 +1,87 @@
+import { Stack } from 'expo-router'
+import { StatusBar } from 'expo-status-bar'
+import { AuthProvider } from '@/contexts/AuthContext'
+import {Tabs} from 'expo-router'
+import { Ionicons } from "@expo/vector-icons";
+import { colors } from '@/constants/theme';
+
+export default function TabLayout() {
+  return (
+    // 
+    <Tabs
+    screenOptions={{
+      headerShown: false,
+
+      tabBarStyle: {
+        backgroundColor: "#6a2e2e00",
+        borderTopWidth: 0,
+        height: 80,
+      },
+
+      tabBarActiveTintColor: colors.primary,
+
+      tabBarInactiveTintColor: "#777",
+    }}
+    >
+      <Tabs.Screen
+      name='home'
+      options={{
+        title:'HomSCreen',
+        tabBarIcon: ({ color, size }) => (
+          <Ionicons
+            name="map"
+            size={size}
+            color={color}
+          />
+        ),
+        
+      }}
+      />
+      <Tabs.Screen
+      name='map'
+      options={{
+        title:"Mapa",
+        tabBarIcon: ({ color, size }) => (
+          <Ionicons
+            name="map"
+            size={size}
+            color={color}
+          />
+        ),
+      }}
+      />
+      <Tabs.Screen
+      name='index'
+      options={{href:null}}
+      />
+      <Tabs.Screen
+      name='login'
+      options={{href:null}}
+      />
+      <Tabs.Screen
+      name='register'
+      options={{href:null}}
+      />
+  <Tabs.Screen
+      name='new-request'
+      options={{href:null}}
+      />
+
+<Tabs.Screen
+      name='users'
+      options={{href:null}}
+      />
+
+<Tabs.Screen
+      name='splash'
+      options={{href:null}}
+      />
+
+<Tabs.Screen
+      name='termos'
+      options={{href:null}}
+      />
+    </Tabs>
+    
+  )
+}
