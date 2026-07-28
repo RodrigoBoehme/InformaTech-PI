9b71022951c095decb069fb8947576890a5ae4cf
RodrigoBoehme <robi.boehme@gmail.com>
2026-07-28 17:57:36 -0300

    Alterado layout para que admin e usuarios comums tenham telas diferentes

--- App/mobile/app/(tabs)/(logged)/_layout.tsx
+++ App/mobile/app/(tabs)/(logged)/_layout.tsx
@@ -4,9 +4,12 @@ import { AuthProvider } from '@/contexts/AuthContext'
 import {Tabs} from 'expo-router'
 import { Ionicons } from "@expo/vector-icons";
 import { colors } from '@/constants/theme';
+import { useAuth } from '@/contexts/AuthContext'
 
 export default function TabLayout() {
-  return (
+  const { user } = useAuth()
+  const isAdmin = user?.role === 'ADMIN'
+  if(isAdmin)return (
     // 
     <AuthProvider>
 
@@ -28,10 +31,110 @@ export default function TabLayout() {
       <Tabs.Screen
       name='home'
       options={{
-        title:'HomSCreen',
+        title:'HomeScreen',
+        tabBarIcon: ({ color, size }) => (
+          <Ionicons
+            name="home"
+            size={size}
+            color={color}
+          />
+        ),
+        
+      }}
+      />
+      <Tabs.Screen
+      name='map'
+      options={{
+        title:"Mapa",
         tabBarIcon: ({ color, size }) => (
           <Ionicons
             name="map"
+            size={size}
+            color={color}
+          />
+        ),
+      }}
+      />
+      
+  <Tabs.Screen
+      name='new-request'
+      options={{href:null}}
+      />
+
+<Tabs.Screen
+      name='users'
+      options={{
+        title:"usuarios",
+        tabBarIcon: ({ color, size }) => (
+          <Ionicons
+            name="people"
+            size={size}
+            color={color}
+          />
+        ),
+      }}
+      />
+<Tabs.Screen
+ name='request/[id]'
+ options={{href:null}}
+/>
+<Tabs.Screen
+name='helps'
+options={{
+  title:"Ajudar",
+  tabBarIcon: ({ color, size }) => (
+          <Ionicons
+            name="medkit"
+            size={size}
+            color={color}
+          />
+        ),
+}}
+/>
+
+<Tabs.Screen
+name='profile'
+options={{
+  title:"Perfil",
+  tabBarIcon: ({ color, size }) => (
+          <Ionicons
+            name="person-outline"
+            size={size}
+            color={color}
+          />
+        ),
+}}
+/>
+
+    </Tabs>
+    </AuthProvider>
+  )
+  else{return(
+    // 
+    <AuthProvider>
+
+    <Tabs
+    screenOptions={{
+      headerShown: false,
+
+      tabBarStyle: {
+        backgroundColor: "#6a2e2e00",
+        borderTopWidth: 0,
+        height: 80,
+      },
+
+      tabBarActiveTintColor: colors.primary,
+
+      tabBarInactiveTintColor: "#777",
+    }}
+    >
+      <Tabs.Screen
+      name='home'
+      options={{
+        title:'HomeScreen',
+        tabBarIcon: ({ color, size }) => (
+          <Ionicons
+            name="home"
             size={size}
             color={color}
           />
@@ -66,8 +169,36 @@ export default function TabLayout() {
  name='request/[id]'
  options={{href:null}}
 />
+<Tabs.Screen
+name='helps'
+options={{
+  title:"Ajudar",
+  tabBarIcon: ({ color, size }) => (
+          <Ionicons
+            name="medkit"
+            size={size}
+            color={color}
+          />
+        ),
+}}
+/>
+
+<Tabs.Screen
+name='profile'
+options={{
+  title:"Perfil",
+  tabBarIcon: ({ color, size }) => (
+          <Ionicons
+            name="person-outline"
+            size={size}
+            color={color}
+          />
+        ),
+}}
+/>
 
     </Tabs>
     </AuthProvider>
   )
+  }
 }
