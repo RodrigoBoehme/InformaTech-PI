b756d12033dc7c3586092f7f7d5968ed1dc4e698
Rodrigo Boehme <11715902963@senacrs.edu.br>
2026-07-30 20:17:52 -0300

    Adicionado background a pagina de registro

--- App/mobile/app/(tabs)/(logged)/users.tsx
+++ App/mobile/app/(tabs)/(logged)/users.tsx
@@ -16,8 +16,9 @@ export default function Users(){
         async function remove(id:string){Alert.alert('Excluir usuário','Esta ação também pode remover dados relacionados.',[{text:'Cancelar'},{text:'Excluir',style:'destructive',onPress:async()=>{await api.delete(`/usuarios/${id}`);load()}}])}
         
         return <Screen scroll={false}>
-            <Background1/>
+            
             <Header title="Usuários" back/>
+            <Background1/>
             <FlatList data={items} keyExtractor={i=>i.id} contentContainerStyle={{gap:12,paddingBottom:30}} renderItem={({item})=><View style={s.card}><Text style={s.title}>{item.name}</Text><Text style={s.meta}>{item.email} • {item.phone}</Text><Text style={s.meta}>Perfil: {item.role} • {item.active?'Ativo':'Inativo'}</Text><View style={{flexDirection:'row',gap:8}}><Button style={{flex:1}} title={item.active?'Desativar':'Ativar'} variant="outline" onPress={()=>toggle(item)}/><Button style={{flex:1}} title="Excluir" variant="danger" onPress={()=>remove(item.id)}/></View></View>}/></Screen>}
 const s=StyleSheet.create(
     {
